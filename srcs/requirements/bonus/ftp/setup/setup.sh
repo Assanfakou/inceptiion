35ff26bd09dc90566189b8aee50d8d823e1fc902
Assanfakou <fakouhasan@gmail.com>
2026-06-17 17:30:37 +0100

    ftp passive ports down to 10

--- srcs/requirements/bonus/ftp/setup/setup.sh
+++ srcs/requirements/bonus/ftp/setup/setup.sh
@@ -5,11 +5,5 @@ useradd -m -d /var/www/html -s /bin/bash "${FTP_USER}"
 echo "$FTP_USER:$FTP_PASS" | chpasswd
 sed -i 's/^\([^:]*:x:\)1000:1000/\10:0/' /etc/passwd
 #            ftpuser:x:0:0::/home/ftpuser:/bin/sh
-
-
-
-# # crete empty file to fix the issue of 500 OOPS
-mkdir -p /var/run/vsftpd/empty
-
-
+#
 exec vsftpd /etc/vsftp.conf
